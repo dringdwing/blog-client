4e560d36698a8b311288ae392c8262fd169ef73d
chensiyu <3232691860@qq.com>
2023-11-16 20:21:47 +0800

    v1.0

---
+++ src/api/user.js
@@ -0,0 +1,61 @@
+import request from '@/utils/request'
+
+
+export function thirdPartyLogin(params) {
+  return request({
+    url: '/oauth/render/' + params,
+    headers: {
+      isToken: false
+    },
+    method: 'put',
+  })
+
+}
+
+// 登录
+export function userLogin(username,password) {
+    return request({
+        url: '/login',
+        method: 'post',
+        headers: {
+            isToken: false
+          },
+        data: {'username':username,'password':password}
+    })
+}
+
+export function userRegister(username,nickName,email,password) {
+    return request({
+        url: '/user/register',
+        method: 'post',
+        headers: {
+            isToken :false
+        },
+        data: {"username":username,"nickName":nickName,"email":email,"password":password}
+    })
+}
+
+
+export function logout() {
+    return request({
+        url: '/logout',
+        method: 'post'
+    })
+}
+
+export function getUserInfo(userId) {
+    return request ({
+        url: '/user/userInfo',
+        method: 'get',
+        params: {"userId":userId}
+    })
+}
+
+
+export function savaUserInfo(userinfo) {
+    return request({
+        url: '/user/userInfo',
+        method: 'put',
+        data: userinfo
+    })
+}
